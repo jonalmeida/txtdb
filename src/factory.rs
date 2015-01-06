fc844e0e7e69a49b5035c1e8d885ed531eef20b2
Jonathan Almeida <git@jonalmeida.com>
2015-01-06 17:52:13 -0500

    Parses a `Record` from a line and returns
    - Added first string_slice() to utils module

--- src/factory.rs
+++ src/factory.rs
@@ -1,4 +1,5 @@
-
+use std::str::FromStr;
+use utils;
 // Each line in the database file is a 'Record' that contains the following data to be read
 // and serialized.
 pub struct Record {
@@ -15,12 +16,24 @@ pub type RecordResult<T, E> = Result<T, E>;
 
 pub trait RecordFactory {
     //TODO Add some factory traits here if necessary
-    fn create(data: String) -> RecordResult<String, String>;
+    fn create(data: String) -> RecordResult<Record, String>;
 }
 
 #[allow(dead_code, unused_must_use, unused_variables)]
-fn create(data: String) -> RecordResult<String, String> {
-    Err("Not implemented yet.".to_string())
+fn create(data: String) -> RecordResult<Record, String> {
+    let vec_of_data = utils::string_slice(data);
+    let id_num = FromStr::from_str(vec_of_data[0].as_slice());
+    let id_value: u64 = match id_num {
+        Some(value)     => value,
+        None            => -1 // This is a failure value
+    };
+
+    Ok(Record {
+        id: id_value,
+        payload: vec_of_data[1].clone(),
+        metadata: vec_of_data[2].clone(),
+    })
+
 }
 
 #[test]
--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,11 @@
 // Any utility functions that are misc can go here
+
+pub fn string_slice(original: String) -> Vec<String> {
+    let vec_of_str: Vec<&str> = original.as_slice().split(' ').collect();
+    let mut vec_of_string: Vec<String> = Vec::new();
+    for x in vec_of_str.iter() {
+        vec_of_string.push(x.to_string());
+    }
+    return vec_of_string;
+}
+

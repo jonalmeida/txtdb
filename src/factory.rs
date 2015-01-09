b78d568b038a2014627e6c85ca50073e90492f70
Jonathan Almeida <git@jonalmeida.com>
2015-01-08 19:23:28 -0500

    New struct `Factory` with empty `new` impl
    - This may go away, but requires some more thought
    - TODO: Add `impl RecordFactory for Factory`

--- src/factory.rs
+++ src/factory.rs
@@ -1,5 +1,7 @@
 use std::str::FromStr;
+use controller::Reader;
 use utils;
+
 // Each line in the database file is a 'Record' that contains the following data to be read
 // and serialized.
 pub struct Record {
@@ -12,13 +14,26 @@ pub struct Record {
     metadata: String,
 }
 
+pub struct Factory {
+    reader: Reader,
+}
+
 pub type RecordResult<T, E> = Result<T, E>;
 
+//TODO Add some factory traits here if necessary
 pub trait RecordFactory {
-    //TODO Add some factory traits here if necessary
     fn create(data: String) -> RecordResult<Record, String>;
 }
 
+impl Factory {
+    pub fn new(reader: Reader) -> Factory {
+        Factory {
+            reader: reader,
+        }
+    }
+}
+
+//TODO Remove the below flag when `create` is used
 #[allow(dead_code, unused_must_use, unused_variables)]
 fn create(data: String) -> RecordResult<Record, String> {
     let vec_of_data = utils::string_slice(data);

3689ea402f49a2a0da0977fb9a73605a6b434794
Jonathan Almeida <git@jonalmeida.com>
2015-01-11 20:30:41 -0500

    Added new test for non-implemented create_from_enc
    - Removed dup `create` that was within the mod

--- src/factory.rs
+++ src/factory.rs
@@ -25,6 +25,8 @@ pub trait RecordFactory {
     fn new(reader: Reader) -> Self;
 
     fn create(&self, data: String) -> RecordResult<Record, String>;
+
+    fn create_from_enc(&self, data: String) -> RecordResult<Record, String>;
 }
 
 impl RecordFactory for Factory {
@@ -50,38 +52,43 @@ impl RecordFactory for Factory {
             metadata: vec_of_data[2].clone(),
         })
     }
-}
 
-//TODO Remove the below flag when `create` is used
-#[allow(dead_code, unused_must_use, unused_variables)]
-fn create(data: String) -> RecordResult<Record, String> {
-    let vec_of_data = utils::string_slice(data);
-    let id_num = FromStr::from_str(vec_of_data[0].as_slice());
-    let id_value: u64 = match id_num {
-        Some(value)     => value,
-        None            => -1 // This is a failure value
-    };
-
-    Ok(Record {
-        id: id_value,
-        payload: vec_of_data[1].clone(),
-        metadata: vec_of_data[2].clone(),
-    })
-
+    fn create_from_enc(&self, data: String) -> RecordResult<Record, String> {
+        Ok(Record {
+            id: 1,
+            payload: String::from_str("Not implemented."),
+            metadata: String::from_str("Not implemented."),
+        })
+    }
 }
 
 #[test]
 fn test_create_record() {
     let input = String::from_str("1 payload metadata");
-    let id_: u64 = 1;
     let expected = Record {
-        id: id_,
+        id: 1,
         payload: String::from_str("payload"),
         metadata: String::from_str("metadata"),
     };
     let reader = Reader::new(Path::new("tests/base-test.txt"));
-    let _factory: Factory = RecordFactory::new(reader);
-    let output: Record = _factory.create(input).ok().expect("Parsing failed.");
+    let factory: Factory = RecordFactory::new(reader);
+    let output: Record = factory.create(input).ok().expect("Parsing failed.");
+    assert_eq!(expected.id, output.id);
+    assert_eq!(expected.payload, output.payload);
+    assert_eq!(expected.metadata, output.metadata);
+}
+
+#[test]
+fn test_create_from_encoded() {
+    let input = String::from_str("5 cGF5bG9hZA== bWV0YWRhdGE=");
+    let expected = Record {
+        id: 5,
+        payload: String::from_str("payload"),
+        metadata: String::from_str("metadata"),
+    };
+    let reader = Reader::new(Path::new("tests/base-test.txt"));
+    let factory: Factory = RecordFactory::new(reader);
+    let output: Record = factory.create_from_enc(input).ok().expect("Parsing failed.");
     assert_eq!(expected.id, output.id);
     assert_eq!(expected.payload, output.payload);
     assert_eq!(expected.metadata, output.metadata);

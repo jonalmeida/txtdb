0b3c393ae7526935adfc2b9c095367fcc7e09885
Jonathan Almeida <git@jonalmeida.com>
2015-01-22 19:15:30 -0500

    Created a new struct Txtdb; moved lib.rs fn there
    (Implementation to follow)

--- src/lib.rs
+++ src/lib.rs
@@ -10,45 +10,53 @@ pub mod controller;
 pub mod factory;
 mod utils;
 
-/// Add a new record to the database. Returns the id of the record added.
-#[allow(dead_code, unused_must_use, unused_variables)]
-fn add<'a, T: ToString>(record: T) -> RecordResult<u64, &'a str> {
-    Ok(1)
+struct Txtdb {
+    factory: factory::Factory,
 }
 
-/// Removes a record with the id provided if it exists.
-/// Returns a `RecordResult` of the record removed.
-#[allow(dead_code, unused_must_use, unused_variables)]
-fn remove_id<'a>(id: u64) -> RecordResult<Record, &'a str> {
-    Err("Not implemented yet")
+impl Txtdb {
+    /// Add a new record to the database. Returns the id of the record added.
+    #[allow(dead_code, unused_must_use, unused_variables)]
+    fn add<'a, T: ToString>(record: T) -> RecordResult<u64, &'a str> {
+        Ok(1)
+    }
+
+    /// Removes a record with the id provided if it exists.
+    /// Returns a `RecordResult` of the record removed.
+    #[allow(dead_code, unused_must_use, unused_variables)]
+    fn remove_id<'a>(id: u64) -> RecordResult<Record, &'a str> {
+        Err("Not implemented yet")
+    }
+
+    /// Finds and removes the first instance of a record that matches the one provided.
+    /// Returns the id of the record it removes.
+    #[allow(dead_code, unused_must_use, unused_variables)]
+    fn remove<'a>(record: Record) -> RecordResult<u64, &'a str> {
+        Err("Not implemented yet")
+    }
+
+    /// Searches for a record with the id provided.
+    /// Returns a copy of the record.
+    #[allow(dead_code, unused_must_use, unused_variables)]
+    fn find_id<'a>(id: u64) -> RecordResult<Record, &'a str> {
+        // 1. Read each line?
+        // 2. Check if the ID matches
+        // 3. Return
+        Err("Not implemented yet")
+    }
+
+    /// Searches for the first instance of a record that matches the one provided.
+    /// Returns the id of the record in the database.
+    #[allow(dead_code, unused_must_use, unused_variables)]
+    fn find<'a>(record: Record) -> RecordResult<u64, &'a str> {
+        // TODO, how do you create a `Record` if you don't know the id?
+        //  Since we aren't using it, should we document not having the id in there?
+        //
+        // 1. Base64 encode the Record
+        // 2. Read each line to find the match encoded value
+        // 3. Return id
+        Err("Not implemented yet")
+    }
 }
 
-/// Finds and removes the first instance of a record that matches the one provided.
-/// Returns the id of the record it removes.
-#[allow(dead_code, unused_must_use, unused_variables)]
-fn remove<'a>(record: Record) -> RecordResult<u64, &'a str> {
-    Err("Not implemented yet")
-}
 
-/// Searches for a record with the id provided.
-/// Returns a copy of the record.
-#[allow(dead_code, unused_must_use, unused_variables)]
-fn find_id<'a>(id: u64) -> RecordResult<Record, &'a str> {
-    // 1. Read each line?
-    // 2. Check if the ID matches
-    // 3. Return
-    Err("Not implemented yet")
-}
-
-/// Searches for the first instance of a record that matches the one provided.
-/// Returns the id of the record in the database.
-#[allow(dead_code, unused_must_use, unused_variables)]
-fn find<'a>(record: Record) -> RecordResult<u64, &'a str> {
-    // TODO, how do you create a `Record` if you don't know the id?
-    //  Since we aren't using it, should we document not having the id in there?
-    //
-    // 1. Base64 encode the Record
-    // 2. Read each line to find the match encoded value
-    // 3. Return id
-    Err("Not implemented yet")
-}

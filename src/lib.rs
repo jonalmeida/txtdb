4ff67b305a4f3518e53859e2a2bbb36b8f35edda
Jonathan Almeida <git@jonalmeida.com>
2015-02-02 19:16:11 -0500

    Changed error type from &str to String
    - Further reading shows that we should prefer String over &str for
      ownership.

--- src/lib.rs
+++ src/lib.rs
@@ -24,28 +24,28 @@ impl Txtdb {
 
     /// Add a new record to the database. Returns the id of the record added.
     #[allow(dead_code, unused_must_use, unused_variables)]
-    fn add<'a,  T: ToString>(&mut self, record: T) -> RecordResult<u64, &'a str> {
+    fn add<T: ToString>(&mut self, record: T) -> RecordResult<u64, String> {
         Ok(1)
     }
 
     /// Removes a record with the id provided if it exists.
     /// Returns a `RecordResult` of the record removed.
     #[allow(dead_code, unused_must_use, unused_variables)]
-    fn remove_id<'a>(&mut self, id: u64) -> RecordResult<Record, &'a str> {
+    fn remove_id(&mut self, id: u64) -> RecordResult<Record, String> {
         Err("Not implemented yet")
     }
 
     /// Finds and removes the first instance of a record that matches the one provided.
     /// Returns the id of the record it removes.
     #[allow(dead_code, unused_must_use, unused_variables)]
-    fn remove<'a>(&mut self, record: Record) -> RecordResult<u64, &'a str> {
+    fn remove(&mut self, record: Record) -> RecordResult<u64, String> {
         Err("Not implemented yet")
     }
 
     /// Searches for a record with the id provided.
     /// Returns a copy of the record.
     #[allow(dead_code, unused_must_use, unused_variables)]
-    fn find_id<'a>(id: u64) -> RecordResult<Record, &'a str> {
+    fn find_id(id: u64) -> RecordResult<Record, String> {
         // 1. Read each line?
         // 2. Check if the ID matches
         // 3. Return
@@ -55,7 +55,7 @@ impl Txtdb {
     /// Searches for the first instance of a record that matches the one provided.
     /// Returns the id of the record in the database.
     #[allow(dead_code, unused_must_use, unused_variables)]
-    fn find<'a>(&self, record: Record) -> RecordResult<u64, &'a str> {
+    fn find(&self, record: Record) -> RecordResult<u64, String> {
         // TODO, how do you create a `Record` if you don't know the id?
         //  Since we aren't using it, should we document not having the id in there?
         //
@@ -65,5 +65,3 @@ impl Txtdb {
         Err("Not implemented yet")
     }
 }
-
-

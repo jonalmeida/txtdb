2dbbded9d3dd2b00b7b94c3739e06bfebbcd3e42
Jonathan Almeida <git@jonalmeida.com>
2015-02-09 00:46:36 -0500

    Moved comments to doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -26,40 +26,42 @@ impl Txtdb {
         }
     }
 
-    /// Add a new record to the database. Returns the id of the record added.
     #[allow(dead_code, unused_must_use, unused_variables)]
     fn add<T: ToString>(&mut self, record: T) -> RecordResult<u64, String> {
+        //! Add a new record to the database. Returns the id of the record added.
         Ok(1)
     }
 
-    /// Removes a record with the id provided if it exists.
-    /// Returns a `RecordResult` of the record removed.
     #[allow(dead_code, unused_must_use, unused_variables)]
     fn remove_id(&mut self, id: u64) -> RecordResult<Record, String> {
+        //! Removes a record with the id provided if it exists.
+        //! Returns a `RecordResult` of the record removed.
         Err("Not implemented yet".to_string())
     }
 
-    /// Finds and removes the first instance of a record that matches the one provided.
-    /// Returns the id of the record it removes.
     #[allow(dead_code, unused_must_use, unused_variables)]
     fn remove(&mut self, record: Record) -> RecordResult<u64, String> {
+        //! Finds and removes the first instance of a record that matches the one provided.
+        //! Returns the id of the record it removes.
         Err("Not implemented yet".to_string())
     }
 
-    /// Searches for a record with the id provided.
-    /// Returns a copy of the record.
     #[allow(dead_code, unused_must_use, unused_variables)]
     fn find_id(id: u64) -> RecordResult<Record, String> {
+        //! Searches for a record with the id provided.
+        //! Returns a copy of the record.
+
         // 1. Read each line?
         // 2. Check if the ID matches
         // 3. Return
         Err("Not implemented yet".to_string())
     }
 
-    /// Searches for the first instance of a record that matches the one provided.
-    /// Returns the id of the record in the database.
     #[allow(dead_code, unused_must_use, unused_variables)]
     fn find(&self, record: Record) -> RecordResult<u64, String> {
+        //! Searches for the first instance of a record that matches the one provided.
+        //! Returns the id of the record in the database.
+
         // TODO, how do you create a `Record` if you don't know the id?
         //  Since we aren't using it, should we document not having the id in there?
         //
